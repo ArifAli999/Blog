38081e9588d0aad2095b961cbc4092b9583d3aa1
Riffyraff <82163631+ArifAli999@users.noreply.github.com>
2022-06-17 17:06:44 +0100

    pusdhh

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,9 @@ const swiperItems = posts.map((p) => {
 
       <><Head>
       <title>My blog</title>
-     <meta name="apple-mobile-web-app-status-bar-style" content="black">
+     <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+     <meta name="theme-color" content="#39ff14"/>
+
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <div className=' bg-black p-0 min-h-screen w-full'>
