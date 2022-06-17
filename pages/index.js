0a4d28f5a17641ee501b30dc5c8d20eaf0a16f7a
Riffyraff <82163631+ArifAli999@users.noreply.github.com>
2022-06-17 17:16:48 +0100

    dixes

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ posts }) {
 
 const swiperItems = posts.map((p) => {
   return (
-    <SwiperSlide key={p.node.slug}  className="   md:h-full md:!w-96  !w-screen ml-2 md:ml-5 md:mt-5 mb-10 flex flex-col gap-0 "
+    <SwiperSlide key={p.node.slug}  className="   md:h-full md:!w-96  !w-screen ml-0 md:ml-5 md:mt-5 mb-10 flex flex-col gap-0 "
   >
    <main className=' w-full bg-red-500 relative'>
       <Image  
@@ -34,13 +34,13 @@ const swiperItems = posts.map((p) => {
       </main>
 
 
-      <main className='w-full flex h-full items-center  justify-between gap-0 md:gap-10 p-0.5 '>
-        <section className='text-center p-2 md:p-3 inline-block'>
-          <span className='text-white text-base md:text-4xl inline-block align-middle'>{moment(p.node.createdAt).format('MM')}</span>
+      <main className=' md:w-full flex h-full items-center   justify-between gap-0 md:gap-10 md:p-1 p-4 '>
+        <section className='text-center p-3 md:p-3 inline-block'>
+          <span className='text-white text-3xl font-black md:text-4xl inline-block align-middle'>{moment(p.node.createdAt).format('MM')}</span>
         </section>
 
-        <section className='w-full h-full flex items-center mb-5  p-2'>
-          <p className='text-sm break-words	inline-block align-middle  text-white'>
+        <section className='w-5/6 md:w-full h-full flex items-center mb-5  p-4'>
+          <p className='text-sm 	 align-middle font-extralight  text-white'>
           {p.node.excerpt}
           </p>
         </section>
@@ -59,11 +59,11 @@ const swiperItems = posts.map((p) => {
 
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <div className=' bg-black p-0 min-h-screen w-full'>
+    <div className=' bg-black p-0 min-h-screen w-full '>
 
         <Swiper
           slidesPerView="auto"
-          spaceBetween={20}
+          spaceBetween={10}
           className='   !grid !grid-flow-col !auto-cols-max p-0 !gap-2 '>
 
           {swiperItems}
