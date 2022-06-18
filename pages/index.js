8a0464150e37f91ce0679d2b2828cb6f2ff70f39
Riffyraff <82163631+ArifAli999@users.noreply.github.com>
2022-06-18 02:48:29 +0100

    purples

--- pages/index.js
+++ pages/index.js
@@ -24,25 +24,28 @@ const swiperItems = posts.map((p) => {
   return (
     <SwiperSlide key={p.node.slug}  className="   md:!h-full md:!w-96 !w-screen ml-0 md:ml-5 md:mt-0  p-0   mb-0 flex flex-col gap-0 "
   >
-   <main className=' w-full   '>
+   <main className=' w-full    relative overflow- bg-no-repeat bg-cover '>
       <Image  
         width={500} height={600} layout="responsive" objectFit="cover"
         src={p.node.featuredImage.url} alt={p.node.title} 
-        className=" md:w-full  object-fill"
+        className=" md:w-full  object-fill rounded"
         quality={100}
-        placeholder="blur"/>
-          
+        />
+                <div class="absolute top-0 right-0 bottom-0 left-0 w-full h-full  bg-fixed opacity-0 hover:opacity-80 transition duration-300 ease-in-out bg-purple-700 flex items-center justify-center">
+                  <p className='text-4xl z-99 p-4 text-purple-50 uppercase font-black text-center '>READ MORE</p>
+                </div>
+
      
       </main>
 
 
-      <main className=' md:w-full flex h-full items-center   justify-between gap-0 md:gap-10 md:p-1 p-4 '>
+      <main className=' md:w-full flex h-full items-center mt-2  justify-between gap-0 md:gap-3 md:p-1 p-4 '>
         <section className='text-center p-3 md:p-3 inline-block'>
-          <span className='text-white text-3xl font-black md:text-4xl inline-block align-middle'>{moment(p.node.createdAt).format('MM')}</span>
+          <span className='text-violet-600 text-3xl font-black  md:text-4xl inline-block align-middle'>{moment(p.node.createdAt).format('MM')}</span>
         </section>
 
-        <section className='w-5/6 md:w-full h-full flex items-center mb-5  p-4'>
-          <p className='text-sm 	 align-middle font-extralight  text-white'>
+        <section className='w-fit md:w-full h-full flex items-center mb-0  md:p-3 p-6'>
+          <p className='text-sm 	 align-middle font-light  font-rale  text-gray-200'>
           {p.node.excerpt}
           </p>
         </section>
@@ -62,9 +65,10 @@ const swiperItems = posts.map((p) => {
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <div className=' bg-black p-0 min-h-screen w-full flex flex-col py-0'>
-    <div className='border-b mb-0 border-gray-600 p-4 w-full flex justify-between items-center'>
+
+    <div className='border-b mb-0 border-gray-600 p-4 w-full flex justify-between items-center shadow-sm drop-shadow-lg shadow-gray-700'>
     
-    <span className='text-purple-100 font-light text-lg'>the   <span className='text-purple-600 font-bold'>&apos;purple&apos;</span> experience</span>
+    <span className='text-purple-100 font-extralight text-lg font-rale group cursor-pointer hover:text-white'>the &nbsp;<span className='text-violet-400 group-hover:text-violet-600 group-hover:-top-1 transition-all duration-500 ease-in-out font-bold relative top-1'>&apos;purple&apos;</span>&nbsp; experience</span>
     </div>
     <div className='p-4'>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'rale': ["Raleway", 'sans-serif'],
+       
+      },
+    },
   },
   plugins: [],
 }
