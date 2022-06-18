fe19e1b373af9cee0592a6e01ce9d2c21e3ea576
Riffyraff <82163631+ArifAli999@users.noreply.github.com>
2022-06-18 03:26:23 +0100

    purples

--- pages/index.js
+++ pages/index.js
@@ -22,30 +22,32 @@ export default function Home({ posts }) {
 
 const swiperItems = posts.map((p) => {
   return (
-    <SwiperSlide key={p.node.slug}  className="   md:!h-full md:!w-96 !w-screen ml-0 md:ml-5 md:mt-0  p-0   mb-0 flex flex-col gap-0 "
+    <SwiperSlide key={p.node.slug}  className=" md:!w-96 !w-screen ml-0 md:ml-5 md:mt-0  p-4   mb-0 flex  flex-col gap-2 "
   >
-   <main className=' w-full    relative overflow- bg-no-repeat bg-cover '>
+   <main className=' w-full    relative h-full bg-no-repeat bg-cover '>
       <Image  
         width={500} height={600} layout="responsive" objectFit="cover"
         src={p.node.featuredImage.url} alt={p.node.title} 
         className=" md:w-full  object-fill rounded"
         quality={100}
         />
-                <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full  bg-fixed opacity-0 hover:opacity-80 transition duration-300 ease-in-out bg-purple-700 flex items-center justify-center">
-                  <p className='text-4xl z-99 p-4 text-purple-50 uppercase font-black text-center '>READ MORE</p>
+                <div className="group absolute top-0 right-0 bottom-0 left-0 w-full h-full  bg-fixed opacity-0 hover:opacity-80 transition duration-300 ease-in-out bg-purple-700 flex items-center justify-center">
+                  <p className='text-4xl z-99 p-4 text-purple-50 uppercase font-black text-center group-hover:-translate-y-6	cursor-pointer hover:text-black opacity-100 transition-all duration-700 '><span className='text-6xl p-2'>&apos;</span>READ MORE 
+                  <span className='text-6xl p-2'>&apos;</span>
+                  </p>
                 </div>
 
      
       </main>
 
 
-      <main className=' md:w-full flex h-full items-center mt-2  justify-between gap-0 md:gap-3 md:p-1 p-4 '>
-        <section className='text-center p-3 md:p-3 inline-block'>
+      <main className='   md:w-full flex h-full items-center justify-between -mt-2 md:mt-0 gap-4 md:gap-3 md:p-1 p-0'>
+        <section className='text-center p-1 md:p-3 inline-block'>
           <span className='text-violet-600 text-3xl font-black  md:text-4xl inline-block align-middle'>{moment(p.node.createdAt).format('MM')}</span>
         </section>
 
-        <section className='w-fit md:w-full h-full flex items-center mb-0  md:p-3 p-6'>
-          <p className='text-sm 	 align-middle font-light  font-rale  text-gray-200'>
+        <section className='w-full break-words  md:w-full h-full flex items-center mt-0  md:p-3 p-2'>
+          <p className='text-sm leading-loose	 align-middle font-light  font-rale  text-gray-200'>
           {p.node.excerpt}
           </p>
         </section>
